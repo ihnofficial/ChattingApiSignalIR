e01a431a65f9f42469ed1ded88d87ce5d78f6493
Ismail Hossain Naim <ihnaimofficial@gmail.com>
2024-09-10 13:51:08 +0600

    Chatting With SignalIR

---
+++ Startup.cs
@@ -0,0 +1,54 @@
+﻿using ChattingServer.Hubs;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace ChattingServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddHttpContextAccessor();
+            services.AddDistributedMemoryCache();
+            services.AddHttpClient();
+            services.AddSignalR();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder => builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization(); // Place UseAuthorization after UseRouting
+            app.UseCors("AllowAll");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chatHub");
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
